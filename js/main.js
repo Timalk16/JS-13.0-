e86f5c3f72841505805d2e605429650de9b667dc
Timofey Malakanchikov <tima160604@mail.ru>
2020-06-11 17:08:50 +0300

    fix errors

--- js/main.js
+++ js/main.js
@@ -2,16 +2,14 @@
 let lang = prompt('Введите язык');
 let namePerson = prompt('Выберите имя: Артем или Максим');
 
-/*
+
 //1 способ
 if (lang === 'ru') {
     console.log('Понедельник Вторник Среда Четверг Пятница Суббота Воскресенье');
 } else if (lang === 'en') {
     console.log('Monday Tuesday Wednesday Thursday Friday Saturday Sunday');
 }
-*/
 //2 способ
-/*
 switch (lang) {
     case 'ru':
         console.log('Понедельник Вторник Среда Четверг Пятница Суббота Воскресенье');
@@ -22,8 +20,6 @@ switch (lang) {
     default:
         console.log('Ошибка');
 }
-*/
-
 //3 спопсоб
 let arr = [
 [ 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
@@ -40,4 +36,14 @@ console.log('result: ', result);
 
 //задание 2
 
-let names = namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент');
+let names = (namePerson === 'Максим') ? console.log('преподаватель') :
+
+(namePerson === 'Артем') ? console.log('директор') :
+
+console.log('студент');
+
+
+
+
+
+
